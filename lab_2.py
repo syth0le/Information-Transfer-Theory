eb967296a7304c1bc77916992ce14d951ae44851
syth0le <chdan565@gmail.com>
2022-11-10 23:55:46 +0300

    added 2nd lab

--- lab_2.py
+++ lab_2.py
@@ -10,7 +10,6 @@ DURATION = 5  # Seconds
 def generate_sine_wave(freq, sample_rate, duration):
     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
     frequencies = x * freq
-    # 2pi because np.sin takes radians
     y = np.sin((2 * np.pi) * frequencies)
     return x, y
 
